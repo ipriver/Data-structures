10e5a6ac9f57cb2a167ff110a3a7ec2d807d259d
ipriver <maxdrozdov18@gmail.com>
2017-05-07 20:36:01 +0300

    js: bt add, min, max tests and func

--- js/bt.js
+++ js/bt.js
@@ -1,31 +1,85 @@
-function BinTree() {
-  this.head = null;
-  this.left = null;
-  this.right = null;
 
-  this.add = function(obj, head=this.head) {
-    if (!head) {
-      head = obj;
-    } else {
-      head.value < obj.value ? (!this.left ? this.left = obj : 0) : (!this.right ? this.right = obj : 0); 
-    }
-  };
-  this.print = function() {
-    console.log(this);
-  };
-};
+function MyException(message) {
+   this.message = message;
+   this.name = 'MyException';
+}
 
 function Node(value) {
-  this.head = null;
+  if (!value) throw new MyException('No value');
   this.value = value;
+  this.left = null;
+  this.right = null;
+}
+
+function BinTree() {
+  this.root = null;
 };
 
-let Tree = new BinTree();
-Tree.add(new Node(8));
-Tree.add(new Node(3));
-Tree.add(new Node(9));
-Tree.add(new Node(2));
-Tree.add(new Node(4));
-Tree.print();
+BinTree.prototype.addNode = function(node) {
+  if (!(node instanceof Node)) throw new MyException('Not a Node');
+  if (!this.root) {
+    this.root = node;
+    return this;
+  }
+  let currentNode = this.root;
+  while (currentNode) {
+    if (currentNode.value >= node.value) {
+      if (currentNode.left) {
+        currentNode = currentNode.left;
+      } else {
+        currentNode.left = node;
+        break;
+      }
+    } else {
+      if (currentNode.right) {
+        currentNode = currentNode.right;
+      } else {
+        currentNode.right = node;
+        break;
+      }
+    }
+  }
+  return this;
+};
 
-module.exports = BinTree;
+BinTree.prototype.countElements = function(currentNode, count=0) {
+  if (!this.root) return 0;
+  count += 1;
+  if (!currentNode) currentNode = this.root;
+  if (!currentNode.left && !currentNode.right) return count;
+  if (!currentNode.left && currentNode.right) return this.countElements(currentNode.right);
+  if (currentNode.left && !currentNode.right) return this.countElements(currentNode.left);
+  return this.countElements(currentNode.left) + this.countElements(currentNode.right);
+};
+
+BinTree.prototype.getMin = function() {
+  if (!this.root) throw new MyException('No elements in BT');
+  let currentNode = this.root;
+  while (currentNode.left) {
+    currentNode = currentNode.left;
+  }
+  return currentNode.value;
+};
+
+BinTree.prototype.getMax = function() {
+  if (!this.root) throw new MyException('No elements in BT');
+  let currentNode = this.root;
+  while (currentNode.right) {
+    currentNode = currentNode.right;
+  }
+  return currentNode.value;
+};
+/*
+let bt = new BinTree();
+bt.addNode(new Node(5));
+bt.addNode(new Node(3));
+bt.addNode(new Node(9));
+bt.addNode(new Node(1));
+
+console.log(bt.countElements())
+*/
+module.exports = {
+  BinTree: BinTree,
+  Node: Node,
+  MyException: MyException
+}
--- js/test/btTests.js
+++ js/test/btTests.js
@@ -1,19 +1,100 @@
-const BinTree = require('../bt');
+const BinTree = require('../bt').BinTree;
+const Node = require('../bt').Node;
+const assert = require('chai').assert;
 const expect = require('chai').expect;
-
-let assert = require('assert');
+const MyException = require('../bt').MyException;
 
 describe('BinaryTree', function() {
   describe('#Bintree', function() {
-    const bt = new BinTree(); 
-    
-    it('check if BinartyTree object', () => {
-      expect(bt).to.be.an.instanceof(BinTree)
+    const bt = new BinTree();     
+    it('check if it\'s a BinaryTree object', function() {
+      assert.instanceOf(bt, BinTree);
     });
-    it('check if BT has head, left, right', () => {
-      expect(bt).to.have.property('head').and.to.be.equal(null);
-      expect(bt).to.have.property('left').and.to.be.equal(null);
-      expect(bt).to.have.property('right').and.to.be.equal(null);
+    it('check if BT has head, left, right', function() {
+      assert.property(bt, 'root');
+      assert.property(bt, 'addNode');
+    });
+  });
+  describe('#Node', function() {
+    const node = new Node(4);
+    it('test node creation instanceOf', function() {
+      assert.instanceOf(node, Node);
+      assert.equal(node.value, 4);
+    });
+    it('node properties check', function() {
+      assert.property(node, 'left');
+      assert.property(node, 'right');
+      assert.property(node, 'value');
+      assert.equal(node.left, null);
+      assert.equal(node.right, null);
+      assert.notEqual(node.value, null);
+    });
+    it('test exception if node has no value provided', function() {
+      expect(() => new Node).to.throw(MyException);
+    });
+  });
+  describe('#addNode', function() {
+    const bt = new BinTree();
+    it('check if it throws error if not a Node passed into function', function() {
+      expect(() => bt.addNode('sdfs')).to.throw(MyException);
+      expect(() => bt.addNode(3)).to.throw(MyException);
+      expect(() => bt.addNode(new Node(4))).not.to.throw(MyException);
+    });
+  });
+  describe('#getMin', function() {
+    it('check if it gets right min value', function() {
+      const bt = new BinTree();
+      bt.addNode(new Node(5));
+      bt.addNode(new Node(8));
+      bt.addNode(new Node(3));
+      bt.addNode(new Node(4));;
+      assert.equal(bt.getMin(), 3);
+    });
+    it('check with more random values', function() {
+      const bt = new BinTree();
+      let numbers = [4, 2, 3, 8, 3, 11, 22, 44, 99, 52, 21, 88, 43];
+      for (let i=0; i < numbers.length; i+=1) {
+        bt.addNode(new Node(numbers[i]));
+      }
+      assert.equal(bt.getMin(), 2);
+    });
+    it('random generator tests', function() {
+      const bt = new BinTree();
+      let min = 1111;
+      for (let i=0; i < 100; i+=1) {
+        x = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
+        bt.addNode(new Node(x));
+        if (x < min) min = x;
+      }
+      assert.equal(bt.getMin(), min);
+    });
+  });
+  describe('#getMax', function() {
+    it('check if it gets right max value', function() {
+      const bt = new BinTree();
+      bt.addNode(new Node(5));
+      bt.addNode(new Node(8));
+      bt.addNode(new Node(3));
+      bt.addNode(new Node(4));;
+      assert.equal(bt.getMax(), 8);
+    });  
+    it('check with more random values', function() {
+      const bt = new BinTree();
+      let numbers = [4, 2, 3, 8, 3, 11, 22, 44, 99, 52, 21, 88, 43];
+      for (let i=0; i < numbers.length; i+=1) {
+        bt.addNode(new Node(numbers[i]));
+      }
+      assert.equal(bt.getMax(), 99);
+    });
+    it('random generator tests', function() {
+      const bt = new BinTree();
+      let max = 0;
+      for (let i=0; i < 100; i+=1) {
+        x = Math.floor(Math.random() * (1000 - 1 + 1)) + 1;
+        bt.addNode(new Node(x));
+        if (x > max) max = x;
+      }
+      assert.equal(bt.getMax(), max);
     });
   });
 });
